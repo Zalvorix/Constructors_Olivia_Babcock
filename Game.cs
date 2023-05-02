b011b4296b9bc8bbcd86c4d0bdbac5cf59951849
Zalvorix <114089240+Zalvorix@users.noreply.github.com>
2023-05-02 09:14:15 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Olivia_Babcock
+{
+    class Game
+    {
+        public string title, genre;
+        /// <summary>
+        /// Defines Game string values
+        /// </summary>
+        /// <param name="titleParam"></param>
+        /// <param name="genreParam"></param>
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {   
+            title = titleParam;
+            genre = genreParam;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Constructors_Olivia_Babcock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Defines myGame values
+            Game myGame = new Game("SW:KotOR", "RPG");
+
+            // Prints myGame title and genre to the console
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre}! ");
+
+            // Defines myOtherGame values
+            Game myOtherGame = new Game();
+
+            // Prints myOtherGame title and genre to the console
+            Console.WriteLine($"My favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game!");
+        }
+    }
+}
